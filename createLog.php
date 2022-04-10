a38f8de2e9e713c66d3f340f084f80e9f8c9cc1a
gozali19 <58578666+zali19@users.noreply.github.com>
2022-04-10 11:18:30 +0700

    Delete createLog.php

--- createLog.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    function createLog($data, $filename, $status, $message) {        
-        $namaFile = 'logs/'.$filename."_".date("Y-m").".txt"; // nama file
-        $dataFile = date("Y-m-d H:i:s")." >> ".$status.", ".$message." >> ".implode(", ",$data); // format response
-        $myfile = file_put_contents($namaFile, $dataFile."\n", 8 | 2); // simpan hasil response kedalam file
-    }
-?>
